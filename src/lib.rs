1bbea8e8f36213ed37664006575bd9eb874240e5
Malique <maliqueauguste@gmail.com>
2024-05-25 13:25:55 -0400

    Implimented gradient descent in tests (basic net), however error doesn't decrease always but sometimes fluctuates

--- src/lib.rs
+++ src/lib.rs
@@ -9,11 +9,11 @@ mod tests {
     #[test]
     fn forward_prop() {
         let input = DMatrix::from_vec(1, 2, vec![1.0, 2.0]);
-        let expected_output = DMatrix::from_element(1, 1, 9.0);
+        let expected_output = DMatrix::from_element(1, 1, 2.75);
 
         let mut nn = NeuralNet::new(ActFunc::ReLU, vec![2, 3, 1]).unwrap();
 
-        println!("{:?}", nn.get_weights());
+        println!("NN: {:?}", nn.get_weights());
 
         nn.forward_propogate(input).unwrap();
 
@@ -22,10 +22,10 @@ mod tests {
 
     #[test]
     fn forward_prop_2() {
-        let input = DMatrix::from_vec(1, 2, vec![-3.0, 2.0]);
-        let expected_output = DMatrix::from_element(1, 1, 0.0);
+        let input = DMatrix::from_vec(1, 2, vec![-6.0, 1.0]);
+        let expected_output = DMatrix::from_element(1, 1, 0.5);
 
-        let mut nn = NeuralNet::new(ActFunc::ReLU, vec![2, 3, 1]).unwrap();
+        let mut nn = NeuralNet::new(ActFunc::ReLU, vec![2, 2, 1]).unwrap();
 
         println!("{:?}", nn.get_weights());
 
@@ -36,8 +36,36 @@ mod tests {
 
     #[test]
     fn matrix() {
-        let m1 = DMatrix::from_vec(2, 3, vec![1, 2, 3, 2, 3, 4]);
-        let m1 = DMatrix::from_vec(2, 3, vec![1, 2, 3, 2, 3, 4]);
+        let m1: DMatrix<i8> = DMatrix::from_vec(1, 3, vec![1, 2, 3]);
+        let m2: DMatrix<i8> = DMatrix::from_vec(1, 4, vec![1, 2, 3, 1]);
+
+
+        let mut input: DMatrix<i8> = m1.clone();
+        let num_columns = input.shape().1;
+        input = input.insert_column(num_columns, 1);
+
+        assert_eq!(m2, input)
+
+    }
+
+    #[test]
+    fn basic_net() {
+        let mut nn = NeuralNet::new(ActFunc::ReLU, vec![2, 2, 1]).unwrap();
+        let input: DMatrix<f64> = DMatrix::from_vec(1, 2, vec![1.0, -3.0]);
+        let expected_output = DMatrix::from_element(1, 1, 4.0);
+        let alpha = 0.5;
+
+        for i in 0..25 {
+            nn.forward_propogate(input.clone()).unwrap();
+            let layer_delta = (nn.cached_output() - expected_output.clone()).sum();
+
+            println!("{}) Output: {:?}, Error: {}", i, nn.cached_output()[0], layer_delta * layer_delta);
+            nn.backward_propogate(layer_delta, alpha).unwrap();
+
+        }
+
+        println!("\n{:?}\n", nn)
+        
 
     }
 }
--- src/neural_net.rs
+++ src/neural_net.rs
@@ -1,6 +1,7 @@
-use std::cmp;
+use std::{cmp, fmt::Debug};
 
 use nalgebra::DMatrix;
+
 
 pub struct NeuralNet {
     act_func: ActFunc,
@@ -18,8 +19,10 @@ impl NeuralNet {
         let mut weights: Vec<DMatrix<f64>> = Vec::new();
 
         for i in 1..shape.len() {
-            //Weights are matricies with a number of rows equal to the number of inputs, and number of columns equal to the number of outputs.
-            weights.push(DMatrix::from_element(shape[i-1], shape[i], 1.0))
+            //Weights are matricies with a number of rows equal to the number of inputs + bias node, and number of columns equal to the number of outputs.
+            weights.push(
+                DMatrix::from_fn(shape[i-1] + 1, shape[i], |i, j| ((i+j + 1) as f64))
+            );
         }
 
         Ok(NeuralNet {
@@ -39,50 +42,84 @@ impl NeuralNet {
         self.layer_output_cache.push(input);
 
         for layer in self.weights.iter() {
+            //extending input to include a bias node
+
+            let mut input: DMatrix<f64> = self.layer_output_cache.last().unwrap().clone();
+            let last_column_index = input.shape().1;
+            input = input.insert_column(last_column_index, 1.0);
+
             self.layer_output_cache.push(
-                self.act_func.apply(self.layer_output_cache.last().unwrap() * layer)
+                self.act_func.apply( input * layer)
             )
         }
 
         Ok(())
     }
 
-    pub fn backward_propogate(&mut self, network_error: f64) -> Result<(), String> {
+    pub fn backward_propogate(&mut self, network_error: f64, alpha: f64) -> Result<(), String> {
         //network_error / delta = average absolute valoe of calculated - expected output
         let alpha = 0.1;
 
         if self.weights.len() != self.layer_output_cache.len() - 1 {
             return Err("Run forward_propogate before backward.".into())
         }
+        else if alpha > 1.0 || alpha < 0.0 {
+            return Err("Alpha must be between zero and 1, as it is used to slow down the chnage in the weights as to not overshoot".into());
+        }
+
+        //println!("Weights: {:?}\nLayer Output: {:?}", self.weights, self.layer_output_cache);
 
         //position of second to last output and of last weights
         let position = self.layer_output_cache.len() - 2;
 
         //dE/dw, change in weights to reduce error = input value sof weights * error of output layer
         let mut weight_deltas: Vec<DMatrix<f64>> = Vec::new();
+
+        //acconting for bias node
+        let mut weight_input = self.layer_output_cache[position].clone();
+        let mut last_column_index = weight_input.shape().1;
+        weight_input = weight_input.insert_column(last_column_index, 1.0);
+
         weight_deltas.push(
             //calculates delta for the last set of weights
-            self.layer_output_cache[position].clone() * network_error
+            (weight_input * network_error).transpose()
         );
 
+
         //finds the error of the outputs of the weights
-        //finds the error of the output layer/nodes right after the weights
-        let mut layer_error: DMatrix<f64> = self.weights[position + 1].clone() * network_error;
+        //finds the error of the output layer/nodes right before the last weights
+        let mut layer_error: DMatrix<f64> = self.weights[position].clone() * network_error;
+        //removes layer error for bias node as bias node
+        let mut last_row_index = layer_error.shape().0 - 1;
+        layer_error = layer_error.remove_row(last_row_index);
 
         //starts off with 2nd to last set of weights
-        for i in (self.weights.len() - 2)..0 {
+        for i in (0..(self.weights.len() - 1)).rev() {
+            //println!("\n{})Layer Err: {:?}\n", i, layer_error);
+
+            //accointing for bias node
+            weight_input = self.layer_output_cache[i].clone();
+            last_column_index = weight_input.shape().1;
+            weight_input = weight_input.insert_column(last_column_index, 1.0);
+
             weight_deltas.push(
                 //calculates delta for the weights
-                self.layer_output_cache[i].clone() * layer_error.clone()
+                (layer_error.clone() * weight_input).transpose()
             );
 
             //calculates delta for the nodes inputing into the current weights
             //In the next iteration of the loop, this will be the error of the output nodes relatvie to the weights 
-            layer_error = self.weights[i].clone() * layer_error
+            layer_error = self.weights[i].clone() * layer_error;
+
+            last_row_index = layer_error.shape().1 - 1;
+            layer_error = layer_error.remove_row(last_row_index);
+
         }
 
+        //println!("\n\nWeight Deltas: {:?}", weight_deltas);
+
         for i in 0..self.weights.len() {
-            self.weights[i] -= weight_deltas.pop().unwrap()
+            self.weights[i] -= weight_deltas.pop().unwrap() * alpha
         }
         
         Ok(())
@@ -101,8 +138,15 @@ impl NeuralNet {
     }
 }
 
+impl Debug for NeuralNet {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Neural Net: \nActivation Function: {:?}\nShape: {:?}\nOutput Cache: {:#?}\nWeights {:?}", self.act_func, self.shape, self.layer_output_cache.last().unwrap()[0], self.weights)
+    }
+}
+
 
 //Activation Funciton
+#[derive(Debug)]
 pub enum ActFunc {
     ReLU
 }
